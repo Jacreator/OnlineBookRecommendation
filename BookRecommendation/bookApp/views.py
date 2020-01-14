9e62cf853460829d523b979c34396c0f2fd8c3dc
Jacreator <jambone.james82@gmail.com>
2020-01-14 23:54:24 +0100

    template well connect to home page

--- BookRecommendation/bookApp/views.py
+++ BookRecommendation/bookApp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+
+    # this is used to hold all the data in a dictionary
+
+    return render(request, 'bookApp/home.html')
